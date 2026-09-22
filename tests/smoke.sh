9c5edb8fe93cd55ce0fb64f278331f170f2e8d7f
kaledindmitrii-oss <kaledin.dmitrii@gmail.com>
2026-09-22 11:22:39 +0400

    Delete tests directory

--- tests/smoke.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-ROOT="$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)"
-sh -n "$ROOT/src/universal-openwrt"
-sh -n "$ROOT/installer/install.sh"
-sh -n "$ROOT/install-luci.sh"
-printf 'smoke: OK\n'
